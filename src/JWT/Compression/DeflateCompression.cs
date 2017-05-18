8054da45e582ad58323f8e82c9ccb15b61c93305
kishore tvk <kishore tvk>
2017-05-17 22:05:42 -0700

    added compression

---
+++ src/JWT/Compression/DeflateCompression.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.IO.Compression;
+using System.Text;
+
+namespace JWT.Compression
+{
+    class DeflateCompression : ICompression
+    {
+        public byte[] CompressData(byte[] inputText)
+        {
+            using (var ms = new MemoryStream())
+            {
+                using (var zip = new GZipStream(ms,
+                                              CompressionMode.Compress))
+                using (var writer = new StreamWriter(zip, Encoding.UTF8))
+                {
+                    writer.Write(inputText);
+                }
+                return ms.ToArray();
+
+            }
+        }
+
+            public string UndoCompressData(byte[] compressedData)
+            {
+                using (var ms = new MemoryStream(compressedData))
+                using (var zip = new GZipStream(ms, CompressionMode.Decompress))
+                using (var sr = new StreamReader(zip, Encoding.UTF8))
+                {
+                    return sr.ReadToEnd();
+
+                }
+
+            }
+        }
+    }
---
+++ src/JWT/Compression/ICompression.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JWT.Compression
+{
+   public interface ICompression
+    {
+        /// <summary>
+        /// interface to get data compression, response compression 
+        /// </summary>
+        /// <param name="inputText">The data to compress.</param>
+       /// <param name="compressedData">The data to de compressed.</param>
+        /// <returns></returns>
+        byte[] CompressData(byte[] inputText);
+        string UndoCompressData(byte[] compressedData);
+    }
+}
